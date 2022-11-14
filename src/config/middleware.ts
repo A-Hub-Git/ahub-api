77ead288774713089c598e44a6f2c972fcc886cb
marwacoder <jibrilmohammed39@gmail.com>
2022-11-14 18:30:05 +0100

    changes to docker

--- src/config/middleware.ts
+++ src/config/middleware.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const REDIS_URL = process.env.REDIS_URL || 'redis';
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_PORT = process.env.REDIS_PORT || 'redis';
 const JWT_SECRET = process.env.JWT_SECRET || 'ahub_secret';
 
 export {REDIS_URL, REDIS_PORT, JWT_SECRET};
