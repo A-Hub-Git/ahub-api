0b3085d9bdf428108be5f1d7fae2653c92cec1cd
marwacoder <jibrilmohammed39@gmail.com>
2022-11-14 16:37:32 +0100

    changes to docker

--- src/Libs/redis.ts
+++ src/Libs/redis.ts
@@ -1,8 +1,8 @@
-import {createClient} from 'redis';
+// import {createClient} from 'redis';
 
-const store = createClient({
-  url: process.env.REDIS_URL
-});
+// const store = createClient({
+//   url: process.env.REDIS_URL
+// });
 
-export default store;
+// export default store;
 //A4abjy5t6c1km25dsca15vnwlbxukexrqn8lgemltb46ikn6gcx;
