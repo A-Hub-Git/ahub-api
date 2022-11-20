e3ff86b25e081465a3abb169913fc1b981693777
marwacoder <jibrilmohammed39@gmail.com>
2022-11-21 00:38:07 +0100

    added otp

--- dist/test/user_test.js
+++ dist/test/user_test.js
@@ -1,9 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const chai_1 = require("chai");
-describe('Account', () => {
-    it('Should run a test for user registration', () => {
-        (0, chai_1.expect)([]);
-    });
+describe('Register', () => {
+    it('Should run a test for user registration', () => { });
 });
 //# sourceMappingURL=user_test.js.map
